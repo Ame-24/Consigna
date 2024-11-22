77f6645c274b537957b930f4a94f01bc69d7ab95
Ame-24 <salome24ys2005@gmail.com>
2024-11-22 07:14:01 -0500

    funciones.h

---
+++ funciones.h
@@ -0,0 +1,25 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#define MAX_PRODUCTOS 50
+#define MAX_RECURSOS 50
+#define LONGITUD_NOMBRE 50
+
+void agregarProducto(char nombres[][LONGITUD_NOMBRE], int tiempos_fabricacion[], char nombres_recursos[][MAX_RECURSOS][LONGITUD_NOMBRE], 
+                     int cantidades_recursos[][MAX_RECURSOS], int cantidades[], int cuentas_recursos[], int *cantidad_productos);
+
+void mostrarProductos(char nombres[][LONGITUD_NOMBRE], int tiempos_fabricacion[], char nombres_recursos[][MAX_RECURSOS][LONGITUD_NOMBRE], 
+                      int cantidades_recursos[][MAX_RECURSOS], int cantidades[], int cuentas_recursos[], int cantidad_productos);
+
+void eliminarProducto(char nombres[][LONGITUD_NOMBRE], int tiempos_fabricacion[], char nombres_recursos[][MAX_RECURSOS][LONGITUD_NOMBRE], 
+                      int cantidades_recursos[][MAX_RECURSOS], int cantidades[], int cuentas_recursos[], int *cantidad_productos);
+
+void editarProducto(char nombres[][LONGITUD_NOMBRE], int tiempos_fabricacion[], int cantidades[], int cantidad_productos);
+
+int calcularTiempoTotal(int tiempos_fabricacion[], int cantidades[], int cantidad_productos);
+
+void verificarFactibilidad(char nombres[][LONGITUD_NOMBRE], int tiempos_fabricacion[], char nombres_recursos[][MAX_RECURSOS][LONGITUD_NOMBRE], 
+                           int cantidades_recursos[][MAX_RECURSOS], int cantidades[], int cuentas_recursos[], int cantidad_productos, 
+                           char nombres_recursos_disponibles[][LONGITUD_NOMBRE], int cantidades_recursos_disponibles[], int cantidad_recursos);
+
+#endif
